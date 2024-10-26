462e8a08ec1cebde6ad829258eead0001008f9bd
Seow Chee Teck <victorseow@Seows-MacBook-Air.local>
2024-10-26 11:18:09 +0800

    Add python code

---
+++ app.py
@@ -0,0 +1,12 @@
+# simple web applications using Flask framework
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, My name is Victor!</p>"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080)
+        
